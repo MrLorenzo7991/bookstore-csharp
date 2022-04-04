6a21e7f2e52b49a39fb01e606b4d0e4ba1b80b83
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-04 16:56:58 +0200

    Completato bonus

---
+++ Bonus/Program.cs
@@ -0,0 +1,80 @@
+﻿Console.WriteLine("Inserisci titolo: ");
+string titolo = Console.ReadLine();
+
+Console.WriteLine("Inserisci autore: ");
+string autore = Console.ReadLine();
+
+Console.WriteLine("Inserisci ISBN: ");
+string isbn = Console.ReadLine();
+
+Console.WriteLine("Inserisci numero di pagine: ");
+int numeroPagine = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci peso del libro in kg: ");
+double pesoLibro = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci larghezza del libro in cm: ");
+double larghezza = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci lunghezza del libro in cm: ");
+double lunghezza = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci profondità del libro in cm: ");
+double profondita = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci valutazione media del libro: ");
+double valutazioneMedia = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il numero di recensioni del libro");
+int numeroRecensioni = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Il libro è disponibile in formato Kindle? (inserisci si o no)");
+string rispostaKindle = Console.ReadLine();
+bool disponibilitaKindle = false;
+if(rispostaKindle == "si")
+{
+    disponibilitaKindle = true;
+}
+
+Console.WriteLine("Il libro è disponibile in con la coperitna flessibile? (inserisci si o no)");
+string rispostaCopertina = Console.ReadLine();
+bool disponibilitaCopertinaFlessibile = false;
+if (rispostaCopertina == "si")
+{
+    disponibilitaCopertinaFlessibile = true;
+}
+
+
+//Stampa informazioni
+//informazioni generiche
+Console.WriteLine("--- IL LIBRO DI OGGI: " + titolo + " di " + autore + " ---");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + isbn);
+Console.WriteLine("Numero delle pagine: " + numeroPagine + " pagine");
+Console.WriteLine("Peso del libro: " + pesoLibro + " kg");
+Console.WriteLine("Dimensioni del libro: " + larghezza + " cm x " + lunghezza + " cm x " + profondita + " cm");
+
+Console.WriteLine();
+
+//informazioni Amazon
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + numeroRecensioni);
+Console.WriteLine("Valutazione media: " + valutazioneMedia + " stelline");
+if (disponibilitaKindle)
+{
+    Console.WriteLine("Kindle disponibile: si");
+}
+else
+{
+    Console.WriteLine("Kindle disponibile: no");
+}
+if (disponibilitaCopertinaFlessibile)
+{
+    Console.WriteLine("Copertina flessibile disponibile: si");
+}
+else
+{
+    Console.WriteLine("Copertina flessibile disponibile: no");
+}
+
+
--- bookstore-csharp/Program.cs
+++ bookstore-csharp/Program.cs
@@ -3,10 +3,41 @@ string titolo = "Clean Code: A Handbook of Agile Software Craftsmanship";
 string autore = "Robert C. Martin";
 string isbn = "978-0132350884";
 int numeroPagine = 431;
-double pesoDelLibro = 660;
-double[] dimensioni = {17.91, 2.54, 23.5};  //larghezza, profondità, lunghezza
+double pesoLibro = 0.66;
+double[] dimensioni = {17.91, 23.5, 2.54};  //larghezza, lunghezza, profondità
 double valutazioneMedia = 4.7;
-int numeroDiRecensioni = 3308;
+int numeroRecensioni = 3308;
 bool disponibilitaKindle = true;
 bool disponibilitaCopertinaFlessibile = true;
 
+//Stampa informazioni
+//informazioni generiche
+Console.WriteLine("--- IL LIBRO DI OGGI: " + titolo + " di " + autore + " ---");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + isbn);
+Console.WriteLine("Numero delle pagine: " + numeroPagine + " pagine");
+Console.WriteLine("Peso del libro: " + pesoLibro + " kg");
+Console.WriteLine("Dimensioni del libro: " + dimensioni[0] + " cm x " + dimensioni[1] + " cm x " + dimensioni[2] + " cm");
+
+Console.WriteLine();
+
+//informazioni Amazon
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + numeroRecensioni);
+Console.WriteLine("Valutazione media: " + valutazioneMedia + " stelline");
+if (disponibilitaKindle)
+{
+    Console.WriteLine("Kindle disponibile: si");
+}
+else
+{
+    Console.WriteLine("Kindle disponibile: no");
+}
+if (disponibilitaCopertinaFlessibile)
+{
+    Console.WriteLine("Copertina flessibile disponibile: si");
+}                     
+else                 
+{                    
+    Console.WriteLine("Copertina flessibile disponibile: no");
+}
