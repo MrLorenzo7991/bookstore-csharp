9165318abd43172c46bdef17b65ace7c8164da6e
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-04 14:39:05 +0200

    Dichiarate variabili

--- bookstore-csharp/Program.cs
+++ bookstore-csharp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//dichiarazione variabili
+string titolo = "Clean Code: A Handbook of Agile Software Craftsmanship";
+string autore = "Robert C. Martin";
+string isbn = "978-0132350884";
+int numeroPagine = 431;
+double pesoDelLibro = 660;
+double[] dimensioni = {17.91, 2.54, 23.5};  //larghezza, profondità, lunghezza
+double valutazioneMedia = 4.7;
+int numeroDiRecensioni = 3308;
+bool disponibilitaKindle = true;
+bool disponibilitaCopertinaFlessibile = true;
+
